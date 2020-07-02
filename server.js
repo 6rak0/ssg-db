c5aeebda78909d86698c0a5ff3944042352a4ecd
6rak0 <grako@6rakbook.local>
2020-07-02 16:24:14 -0500

    cambio de directorio local

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ mongoose.connect(mongoURI, {
 app.use('/api/reportes', reportesRoutes)
 
 if(process.env.NODE_ENV === 'production'){
-  app.use(express.static('client/public'))
+  app.use(express.static('client/build'))
   app.get('*', (req,res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   })
